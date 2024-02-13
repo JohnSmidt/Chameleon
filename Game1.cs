edef0da1daa3277e0348550cb758cee5a895fc0e
John Smidt <johnmsmidt@gmail.com>
2024-02-12 22:24:21 -0700

    checking to see if git config is working correctly so I can see my green squares, and get my fair share of dopamine

--- Game1.cs
+++ Game1.cs
@@ -37,10 +37,12 @@ namespace GameEngine
 
             for (int i = 0; i < 10; i++)
             {
-                
                     Debug.WriteLine(rng.percentage());
-               
             }
+
+            int[] temp = new int[10] {1,2,3,4,5,6,7,8,9,10};
+
+            Debug.WriteLine(rng.pickFromArray(temp));
                
 
             base.Initialize();
